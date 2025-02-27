e0cc701a791aa1656dbb7e6a89ebde760bbbe0c1
LifeDeLicious <67972672+LifeDeLicious@users.noreply.github.com>
2025-02-27 15:39:41 +0200

    frontend basic tanstack router & query setup

---
+++ frontend/src/App.jsx
@@ -0,0 +1,21 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+
+import { RouterProvider, createRouter } from "@tanstack/react-router";
+import { routeTree } from "./routeTree.gen.ts";
+
+const router = createRouter({ routeTree });
+
+function App() {
+  const [count, setCount] = useState(0);
+
+  return (
+    <>
+      <RouterProvider router={router} />
+    </>
+  );
+}
+
+export default App;
