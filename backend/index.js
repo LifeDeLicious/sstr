6f4e56d7b5f82e1dc28abd81ba2366afdb9a12a9
LifeDeLicious <67972672+LifeDeLicious@users.noreply.github.com>
2025-04-17 11:22:45 +0300

    testing c# apis

--- backend/index.js
+++ backend/index.js
@@ -1,15 +1,17 @@
 import express from "express";
 
+import { db } from "./db/index.js";
+
 const app = express();
 
 const port = 3000;
 
 //not needed if frontend is hosted it seems like!
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // allowing localhost frontned to request
-  res.header("Access-Control-Allow-Methods", "GET, POST");
-  res.header("Access-Control-Allow-Headers", "Content-Type");
-  next();
+  // res.header("Access-Control-Allow-Origin", "http://localhost:5173"); // allowing localhost frontned to request
+  // res.header("Access-Control-Allow-Methods", "GET, POST");
+  // res.header("Access-Control-Allow-Headers", "Content-Type");
+  // next();
 });
 
 app.listen(port, () => {
@@ -25,4 +27,10 @@ app.get("/", (req, res) => {
 app.get("/laps", (req, res) => {
   console.log("laps page");
   res.json({ message: "amongus" });
+});
+
+app.post("/testpost", (req, res) => {
+  const data = req.body;
+
+  console.log(`request: ${JSON.stringify(data)}`);
 });
