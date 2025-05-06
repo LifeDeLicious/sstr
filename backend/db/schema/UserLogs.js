8eeb295b68dde44d10a2266b1281f4ed2a150c35
LifeDeLicious <67972672+LifeDeLicious@users.noreply.github.com>
2025-05-06 11:14:55 +0300

    v0.3.1.3 fixed naming userlogs.js

--- backend/db/schema/UserLogs.js
+++ backend/db/schema/UserLogs.js
@@ -1,7 +1,7 @@
 import { mysqlTable, varchar, int, timestamp } from "drizzle-orm/mysql-core";
 import { Users } from "./Users.js";
 
-export const Analysis = mysqlTable("Analysis", {
+export const UserLogs = mysqlTable("UserLogs", {
   UserID: int("UserID").references(() => Users.UserID),
   EventType: varchar("EventType", { length: 50 }).notNull(),
   EventTimestamp: timestamp("AnalysisDate").defaultNow(),
