db249238415b1976354ce5425a9c401cccc1db61
LifeDeLicious <67972672+LifeDeLicious@users.noreply.github.com>
2025-05-07 23:06:56 +0300

    cors

--- backend/index.js
+++ backend/index.js
@@ -36,6 +36,14 @@ app.use(cookieParser());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(express.json());
 
+app.options("*", (req, res) => {
+  res.header("Access-Control-Allow-Origin", "https://sstr.reinis.space");
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.header("Access-Control-Allow-Credentials", "true");
+  res.status(204).send();
+});
+
 app.listen(port, () => {
   console.log("listening on port 3000");
 });
