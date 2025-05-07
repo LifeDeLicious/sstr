db22d06dcdd6213bfd408832b099b499be87dfe4
LifeDeLicious <67972672+LifeDeLicious@users.noreply.github.com>
2025-05-07 14:01:14 +0300

    testing lap uploads

--- backend/controllers/lapController.js
+++ backend/controllers/lapController.js
@@ -3,24 +3,55 @@ import { desc, or, eq } from "drizzle-orm";
 import { Laps } from "../db/schema/Laps.js";
 import { Users } from "../db/schema/Users.js";
 import { Sessions } from "../db/schema/Sessions.js";
+import { uploadTelemetryFile } from "./fileUpload.js";
 
 const postLap = async (req, res) => {
   const { sessionID, userID, isFastestLap, telemetryData, lapTime } = req.body;
 
-  //   try {
-  //     const session = await db.insert(Sessions).values({});
-  //   } catch (error) {
-  //     console.log("error :", error);
-  //   }
+  try {
+    const lap = await db
+      .insert(Laps)
+      .values({
+        UserID: userID,
+        LapTime: lapTime,
+        SessionID: sessionID,
+        IsFastestLapOfSession: isFastestLap,
+      })
+      .$returningId();
 
-  console.log(`session id: ${sessionID}`);
-  console.log(`userid: ${userID}`);
-  console.log(`is fastestlap: ${isFastestLap}`);
-  console.log(`laptime: ${lapTime}`);
-  res.status(200).json({
-    success: true,
-    message: "Lap Data Received!",
-  });
+    const lapID = lap[0].LapID;
+    console.log("inserted lap: ", lapID);
+    const lapFileKey = `/laps/lap-${lapID}`;
+
+    uploadTelemetryFile(lapID, userID, telemetryData);
+
+    const lapUpdate = await db
+      .update(Laps)
+      .set({
+        LapFileKey: lapFileKey,
+      })
+      .where(eq(Laps.LapID, lapID))
+      .$returningId();
+
+    console.log("updated lapid: ", lapUpdate[0].LapID);
+    //!db insertott apla keyu - /laps/lap-{lapid}
+    //db insert lap
+    //returningid
+    //!db insertot lapfilekey - /laps/lap-{lapid}
+
+    //uploadTelemetryFile ( apla id, userID, telemetryData )??
+
+    console.log(`session id: ${sessionID}`);
+    console.log(`userid: ${userID}`);
+    console.log(`is fastestlap: ${isFastestLap}`);
+    console.log(`laptime: ${lapTime}`);
+    res.status(200).json({
+      success: true,
+      message: "Lap Data Received!",
+    });
+  } catch (err) {
+    console.log(err);
+  }
 };
 
 const lapController = {
--- backend/db/schema/Laps.js
+++ backend/db/schema/Laps.js
@@ -11,9 +11,9 @@ import { Sessions } from "./Sessions.js";
 export const Laps = mysqlTable("Laps", {
   LapID: int("LapID").primaryKey().autoincrement().notNull(),
   UserID: int("UserID").references(() => Users.UserID),
-  LapFileName: varchar("LapFileName", { length: 255 }).notNull(),
+  LapFileKey: varchar("LapFileKey", { length: 255 }),
   LapTime: double("LapTime").notNull(),
   SessionID: int("SessionID").references(() => Sessions.SessionID),
   IsFastestLapOfSession: boolean("IsFastestLapOfSession"),
-  LapOfSession: int("LapOfSession").notNull(),
+  //LapOfSession: int("LapOfSession").notNull(),
 });
