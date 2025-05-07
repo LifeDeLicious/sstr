6556155f9d42a9cb13cf8675041a99020032b1c1
LifeDeLicious <67972672+LifeDeLicious@users.noreply.github.com>
2025-05-07 23:00:47 +0300

    cors

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,7 @@ const port = 3000;
 
 app.use(
   cors({
-    origin: ["https://sstr.reinis.space", "http://localhost:5173"],
+    origin: "https://sstr.reinis.space",
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
