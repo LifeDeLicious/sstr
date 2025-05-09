b5d7c7c82f4aac4b966732b54534273f14202de4
LifeDeLicious <67972672+LifeDeLicious@users.noreply.github.com>
2025-05-09 22:19:05 +0300

    router preload test

--- frontend/src/routes/session.$sessionId.jsx
+++ frontend/src/routes/session.$sessionId.jsx
@@ -6,7 +6,7 @@ export const Route = createFileRoute("/session/$sessionId")({
   // loader: async ({ params }) => {
   //   return fetchPost(params.sessionId);
   // },
-  preload: "intent",
+  defaultPreload: "intent",
   component: RouteComponent,
 });
 
