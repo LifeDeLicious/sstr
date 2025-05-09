daeed1e409fc1b9f2dfffcbf7b39ee603cf9d691
LifeDeLicious <67972672+LifeDeLicious@users.noreply.github.com>
2025-05-09 23:21:47 +0300

    get /session/data v1.2

--- frontend/src/routes/session.$sessionId.jsx
+++ frontend/src/routes/session.$sessionId.jsx
@@ -14,6 +14,7 @@ export const Route = createFileRoute("/session/$sessionID")({
 function RouteComponent() {
   const { user, loading } = useAuth();
   const { sessionID } = Route.useParams();
+  console.log(sessionID);
 
   const { data: sessionData, isLoading: sessionLoading } = useQuery({
     queryKey: ["sessionData", user?.userID],
