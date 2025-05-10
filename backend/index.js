4cb7160f99b4589e511309acb3efa10ef6d74f9f
LifeDeLicious <67972672+LifeDeLicious@users.noreply.github.com>
2025-05-10 23:50:31 +0300

    testing locally

--- backend/index.js
+++ backend/index.js
@@ -16,7 +16,11 @@ const port = 3000;
 
 app.use(
   cors({
-    origin: ["https://sstr.reinis.space", "https://api.sstr.reinis.space"],
+    origin: [
+      "https://sstr.reinis.space",
+      "https://api.sstr.reinis.space",
+      "http://localhost:5173",
+    ],
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
