461f68d49881716f70d56ec7bc10ac2dc2463339
LifeDeLicious <67972672+LifeDeLicious@users.noreply.github.com>
2025-05-13 17:41:15 +0300

    getgraphdata v3

---
+++ backend/controllers/fileOperations.js
@@ -0,0 +1,104 @@
+import { GetObjectCommand, PutObjectCommand, S3 } from "@aws-sdk/client-s3";
+
+const s3Client = new S3({
+  forcePathStyle: false,
+  endpoint: "https://fra1.digitaloceanspaces.com",
+  region: "fra1",
+  credentials: {
+    accessKeyId: process.env.SPACES_KEY,
+    secretAccessKey: process.env.SPACES_SECRET,
+  },
+});
+
+export async function getTelemetryFile(fileKey) {
+  console.log("getteleemtryfile: ", fileKey);
+
+  const key = `${fileKey}.json`;
+
+  const params = {
+    Bucket: "sstr-laps",
+    Key: key,
+  };
+
+  return await fetchObject(params);
+}
+
+const fetchObject = async (params) => {
+  try {
+    const data = await s3Client.send(new GetObjectCommand(params));
+
+    const streamToString = async (stream) => {
+      const chunks = [];
+      for await (const chunk of stream) {
+        chunks.push(chunk);
+      }
+      return Buffer.concat(chunks).toString("utf-8");
+    };
+
+    const bodyString = await streamToString(data.Body);
+
+    const jsonData = JSON.parse(bodyString);
+
+    return jsonData;
+  } catch (error) {
+    console.log("fileoperations getobject error: ", error);
+  }
+};
+
+// const fetchObject = async (params) => {
+//   try {
+//     const data = await s3Client.send(new GetObjectCommand(params));
+
+//     const streamToString = async (stream) => {
+//       const chunks = [];
+//       for await (const chunk of stream) {
+//         chunks.push(chunk);
+//       }
+//       return Buffer.concat(chunks).toString("utf-8");
+//     };
+
+//     const bodyString = await streamToString(data.Body);
+
+//     const jsonData = JSON.parse(bodyString);
+
+//     console.log("received json", jsonData);
+//     return data;
+//   } catch (error) {
+//     console.log("fileoperations getobject error: ", error);
+//   }
+// };
+
+export default async function uploadTelemetryFile(
+  lapID,
+  userID,
+  telemetryData
+) {
+  console.log("amongus");
+
+  const key = `laps/lap-${lapID}.json`;
+
+  const params = {
+    Bucket: "sstr-laps",
+    Key: key,
+    Body: Buffer.from(JSON.stringify(telemetryData), "utf-8"),
+    Metadata: {
+      "x-amz-meta-my-key": key,
+      userid: userID,
+    },
+  };
+
+  return await uploadObject(params);
+}
+
+const uploadObject = async (params) => {
+  try {
+    const data = await s3Client.send(new PutObjectCommand(params));
+    console.log(
+      "successfully uploaded object: " + params.Bucket + "/" + params.Key
+    );
+    return data;
+  } catch (err) {
+    console.log("error : ", err);
+    throw err;
+  }
+};
