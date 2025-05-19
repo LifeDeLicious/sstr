93d81348704fd97a10953ec453772cb3e760431c
LifeDeLicious <67972672+LifeDeLicious@users.noreply.github.com>
2025-05-19 16:41:01 +0300

    navbar admin pages removed v4

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -67,9 +67,9 @@ export default function Navbar() {
               {user.IsAdmin ? (
                 <>
                   <div className="divider mt-0 mb-0"></div>
-                  <li>
-                    <Link to={"/admin"}>Admin panel</Link>
-                  </li>
+                  <Link className="ml-1.5" to={"/admin"}>
+                    Admin panel
+                  </Link>
                 </>
               ) : (
                 <></>
