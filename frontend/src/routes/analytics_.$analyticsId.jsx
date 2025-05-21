027f5d16d143bdcd985f76dfe259d344f971f9f5
LifeDeLicious <67972672+LifeDeLicious@users.noreply.github.com>
2025-05-21 10:21:39 +0300

    analysis name editable v4

--- frontend/src/routes/analytics_.$analyticsId.jsx
+++ frontend/src/routes/analytics_.$analyticsId.jsx
@@ -282,7 +282,7 @@ function RouteComponent() {
             {/* <h1 className="text-3xl mb-8 col-span-4">
               {"Analysis name inside a textbox/outline"}
             </h1> */}
-            <div className="col-span-2">
+            <div className="col-span-2 mr-8">
               {isEditingName ? (
                 <input
                   ref={nameInputRef}
