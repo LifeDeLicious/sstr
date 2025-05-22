63b113502639c5dc7a29e4652a7b3445f249763b
LifeDeLicious <67972672+LifeDeLicious@users.noreply.github.com>
2025-05-23 00:16:37 +0300

    final? v2

--- frontend/src/routes/__root.jsx
+++ frontend/src/routes/__root.jsx
@@ -1,8 +1,6 @@
 import { createRootRoute, Link, Outlet } from "@tanstack/react-router";
 import { TanStackRouterDevtools } from "@tanstack/router-devtools";
-import { List } from "../components/List";
 import Navbar from "../components/Navbar";
-import Accordion from "../components/Accordion";
 import { useAuth } from "../context/AuthContext.jsx";
 
 export const Route = createRootRoute({
